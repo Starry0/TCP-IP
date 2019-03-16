4bffcbd1e0eeff7ac560ef7042334c07388156bb
Starry <1173298382@qq.com>
2019-03-16 14:25:47 +0800

    2019-3-16

--- 3.地址族与数据序列/endian_conv.cpp
+++ 3.地址族与数据序列/endian_conv.cpp
@@ -6,5 +6,18 @@
 #include <arpa/inet.h>
 
 int main(int argc, char* argv[]) {
+    unsigned short host_port = 0x1234;
+    unsigned short net_port;
+    unsigned long host_addar = 0x12345678;
+    unsigned long net_addr;
 
+    net_port = htons(host_port);
+    net_addr = htonl(host_addar);
+
+    printf("Host ordred post:%#x \n",host_port);
+    printf("Network ordred post:%#x \n",net_port);
+    printf("Host ordred address:%#x \n",host_addar);
+    printf("Network ordred address:%#x \n",net_addr);
+
+    return 0;
 }
